662b581c7a314a45219fbda1ed8f6cf6067adfc8
Akshay Panchal <akshay.panchalhp@gmail.com>
2023-02-22 17:19:12 -0500

    add demo springboot project

---
+++ demo/src/main/java/com/akshayp/demo/SpringecommerceApplication.java
@@ -0,0 +1,13 @@
+package com.akshayp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringecommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringecommerceApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/akshayp/demo/SpringecommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshayp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringecommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
